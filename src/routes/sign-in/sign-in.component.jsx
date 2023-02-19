138643c41a1f727a7a6a5bf950aeecac1eb92599
Ummidono <Crookstonjo@hotmail.com>
2023-02-19 14:07:11 -0700

    message

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,20 @@
 import { signInWithGooglePopup, createUserDocFromAuth } from "../../utils/firebase/firebase.utils";
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
+
+//for Redirect
+//import { useEffect } from "react";
+//import { getRedirectResult } from "firebase/auth";
+//import { auth, signInWithGoogleRedirect} from "../../utils/firebase/firebase.utils";
 
 const SignIn = () =>{
 
+    // useEffect(async () => {
+    //     const response = await getRedirectResult(auth);
+    //     if(response)
+    //     {
+    //         const userDocRef = await createUserDocFromAuth(response.user);
+    //     }
+    // },[] );
     const logGoogleUser = async () =>
     {
         const {user} = await signInWithGooglePopup();
@@ -12,6 +25,9 @@ const SignIn = () =>{
         <div>
             <h1>Sign in Page</h1>
             <button onClick = {logGoogleUser}>Sign in with Google Popup</button>
+            <SignUpForm/>
+
+            {/* <button onClick = {signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
         </div>
     );
 };
