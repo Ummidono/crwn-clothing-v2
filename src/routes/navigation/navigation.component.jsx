047107119cf83632ddc302336784492420bb50cf
Ummidono <Crookstonjo@hotmail.com>
2023-02-28 17:47:48 -0700

    added cart

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,10 +4,14 @@ import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss'
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../contexts/cart.context";
 
 const Navigation = () => {
 
   const {currentUser} = useContext(UserContext);
+  const {isCartOpen} = useContext(CartContext);
     return (
       <Fragment>
         <div className='navigation'>
@@ -23,8 +27,9 @@ const Navigation = () => {
               (<span className='nav-link' onClick={signOutUser}>Sign Out</span>):
               (<Link className='nav-link' to='/auth'>Sign-in</Link>)
             }
-            
+            <CartIcon/>
           </div>
+          {isCartOpen && <CartDropdown/>}
         </div>
         <Outlet/>
       </Fragment>
